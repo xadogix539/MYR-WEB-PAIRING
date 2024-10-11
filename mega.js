a5f3fb73c18d612cd712dce231fa15654beba84b
Iron Man <narendrannirmal6753@gmail.com>
2024-10-11 15:19:17 +0530

    Create mega.js

---
+++ mega.js
@@ -0,0 +1,36 @@
+const mega = require('megajs');
+const crypto = require('crypto');
+const path = require('path');
+const config = require('./config.js');
+
+function generateUA() {
+    var browsers = ['Chrome', 'Firefox', 'Safari', 'Edge'];
+    var versions = ['91.0', '90.0', '89.0', '88.0'];
+    var os = ['Windows NT 10.0', 'Macintosh; Intel Mac OS X 10_15_7', 'Linux; Ubuntu 20.04'];
+    var browser = browsers[Math.floor(Math.random() * browsers.length)];
+    var version = versions[Math.floor(Math.random() * versions.length)];
+    var platform = os[Math.floor(Math.random() * os.length)];
+    return `Mozilla/5.0 (${platform}) AppleWebKit/537.36 (KHTML, like Gecko) ${browser}/${version} Safari/537.36`;
+}
+
+var auth = {
+    email: config.EMAIL,
+    password: config.PASS,
+    userAgent: generateUA()
+};
+
+var upload = (pth) => {
+    return new Promise((resolve, reject) => {
+        var myre = `${crypto.randomBytes(5).toString('hex')}${path.extname(pth)}`;
+        var storage = new mega.Storage(auth, () => {
+            var Json = require(pth);
+            var Content = Buffer.from(JSON.stringify(Json));
+            var stream = storage.upload({ name: myre, size: Content.length, allowUploadBuffering: true });
+            stream.end(Content);
+            stream.on('complete', (file) => file.link((err, url) => err ? reject(err) : resolve(url)));
+            stream.on('error', (error) => reject(error));
+        });
+    });
+};
+
+module.exports = { upload };
